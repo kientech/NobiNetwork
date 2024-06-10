7e9588661b071a5fbc609c616de3da8648914deb
Coding With Kien <duongtrungkien.03052003@gmail.com>
2024-06-10 12:41:12 +0700

    update image

--- src/pages/auth/AuthPage.jsx
+++ src/pages/auth/AuthPage.jsx
@@ -12,26 +12,14 @@ const AuthPage = () => {
       <Container maxW={"container.md"} p={"0"}>
         <Flex justifyContent={"center"} alignItems={"center"} gap={"10"}>
           <Box display={{ base: "none", md: "block" }}>
-            <Image
-              src="/public/images/auth.png"
-              h={650}
-              alt="Phone NobiNetwork"
-            />
+            <Image src="images/auth.png" h={650} alt="Phone NobiNetwork" />
           </Box>
           <VStack spacing={4} align={"stretch"}>
             <AuthForm></AuthForm>
             <Box textAlign={"center"}>Get the app</Box>
             <Flex gap={"5"} justifyContent={"center"}>
-              <Image
-                src="/public/images/playstore.png"
-                h={"10"}
-                alt="PlayStore"
-              />
-              <Image
-                src="/public/images/microsoft.png"
-                h={"10"}
-                alt="Microsoft"
-              />
+              <Image src="images/playstore.png" h={"10"} alt="PlayStore" />
+              <Image src="images/microsoft.png" h={"10"} alt="Microsoft" />
             </Flex>
           </VStack>
         </Flex>
